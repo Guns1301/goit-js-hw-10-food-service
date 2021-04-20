e524aaf731ecce57f779bb028f572178b6928ca1
Guns1301 <67137938+Guns1301@users.noreply.github.com>
2021-04-21 00:03:52 +0300

    fix DRY

--- src/js/change-theme.js
+++ src/js/change-theme.js
@@ -1,7 +1,7 @@
 const refs = {
-    body: document.querySelector('body'),
-    switch: document.querySelector('.theme-switch__toggle'),
-}
+  body: document.querySelector('body'),
+  switch: document.querySelector('.theme-switch__toggle'),
+};
 
 const Theme = {
   LIGHT: 'light-theme',
@@ -13,17 +13,23 @@ console.dir(refs.switch);
 refs.switch.addEventListener('change', changeColorTheme);
 // console.dir(refs.switch);
 
+const replaceTheme = (add, remove) => {
+  //функция изменяющая тему
+  refs.body.classList.add(add);
+  refs.body.classList.remove(remove);
+};
+
 function changeColorTheme() {
-    const checked = refs.switch.checked;
-    if (checked) {
-        refs.body.classList.add(Theme.DARK);
-        refs.body.classList.remove(Theme.LIGHT);
-        localStorage.setItem('theme', Theme.DARK);
-    } else {
-        refs.body.classList.add(Theme.LIGHT);
-        refs.body.classList.remove(Theme.DARK);
-        localStorage.setItem('theme', Theme.LIGHT);
-    }   
+  const checked = refs.switch.checked;
+  if (checked) {
+    replaceTheme(Theme.DARK, Theme.LIGHT); // вызов функции изменяющей тему
+
+    localStorage.setItem('theme', Theme.DARK);
+  } else {
+    replaceTheme(Theme.LIGHT, Theme.DARK);
+
+    localStorage.setItem('theme', Theme.LIGHT);
+  }
 }
 
 // Добавляем класс на боди и перемещаем переключатель, если в localStorage была темная тема
@@ -31,10 +37,8 @@ const localTheme = localStorage.getItem('theme');
 addClassForLocalTheme();
 
 function addClassForLocalTheme() {
-
-    if (localTheme === Theme.DARK) {
-        refs.body.classList.add(Theme.DARK);
-        refs.switch.checked = true;
-    }
-
+  if (localTheme === Theme.DARK) {
+    refs.body.classList.add(Theme.DARK);
+    refs.switch.checked = true;
+  }
 }
